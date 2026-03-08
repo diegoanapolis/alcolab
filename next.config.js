76dd31629502391467a2e34454ae87d606180d32
diegoanapolis <134946667+diegoanapolis@users.noreply.github.com>
2026-03-08 18:20:19 -0300

    chore: atualiza projeto (rodada 21)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    quality: 95,
-  },
   experimental: {
     // keep defaults; add flags if your main project has them
   }
